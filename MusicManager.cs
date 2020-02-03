da7fd50c85029a7da35f3593796b9472e50984cb
loguntetley <48089813+loguntetley@users.noreply.github.com>
2020-02-03 13:41:19 +0000

    Code Snippets of my Game Jam work
    code snippets

---
+++ MusicManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(AudioSource))]
+public class MusicManager : MonoBehaviour
+{
+    public AudioSource[] audioTracks;
+    public Animator musicAnim, musicAnim2;
+    public string levelOneName, levelTwoName;
+
+    private void Awake()
+    {
+        audioTracks[0].Play();
+        audioTracks[1].Play();
+        SceneManager.activeSceneChanged += SceneChanged;
+    }
+
+    private void SceneChanged(Scene prev, Scene current)
+    {
+        if (current.name == levelOneName)
+        {
+            musicAnim.SetTrigger("fadeOut");
+            musicAnim2.SetTrigger("fadeIn2");
+        }
+        if (current.name == levelTwoName)
+        {
+            musicAnim.SetTrigger("fadeIn");
+            musicAnim2.SetTrigger("fadeOut2");
+        }
+        Debug.Log(current.name);
+    }
+
+}
